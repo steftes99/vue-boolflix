330d5c21ffd954e2c299cb56526509551038f0ee
steftes99 <stefanotescione83@gmail.com>
2021-07-12 15:30:20 +0200

    grafica base searchbar e button

---
+++ boolflix/src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+         <input type="text">
+        <button>Search</button>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
